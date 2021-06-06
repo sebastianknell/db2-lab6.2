d3864e8ec5ed4f7cdc062917c546659b8f70cc0d
Sebastian Knell <sebastianknell@hotmail.com>
2021-06-05 21:06:52 -0500

    preprocesamiento

--- invertedIndex.py
+++ invertedIndex.py
@@ -1,8 +1,30 @@
-from typing import Sequence
 import nltk
-nltk.download("punkt")
+# nltk.download("punkt")
+# nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from nltk.util import pr
 
-text = "hola. me llamo sebastian"
+files = ["libro{}.txt".format(i) for i in range(1, 7)]
+
+# stoplist.txt tiene tildes
+stoplist = stopwords.words('spanish')
+stoplist += ['.','?','-','«', '»', ',']
+
+with open("docs/{}".format(files[0])) as file:
+    lines = file.readlines()
+    text = ""
+    for line in lines:
+        text += line.lower().strip()
+
 words = nltk.word_tokenize(text)
+for token in words:
+    if token in stoplist:
+        words.remove(token)
+
+# print(words)
+stemmer = SnowballStemmer('spanish')
+for i in range(len(words)):
+    words[i] = stemmer.stem(words[i])
 
 print(words)
